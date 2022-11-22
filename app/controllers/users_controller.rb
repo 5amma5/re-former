b9f9c545ccc8359f4ee5c4f2969aca00cd36f711
5amma5 <sam.anders5@gmail.com>
2022-11-22 06:10:14 -0700

    Stage 3. Revised form where it uses form_with helper. Updated UsersController#create so that it accepts nested User attributes again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new()
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    # @user = User.new(user_params)
+    # accept top level User attributes
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    
+    # accept nested User attributes
+    @user = User.new(user_params)
+    
     if @user.save
       redirect_to new_user_path
     else
